b1527a1b2b64b51a0355da4fb34b3cc4950ced69
Oleg Korchagin <OKorchagin@revelsystems.com>
2016-01-02 21:28:37 +0300

    new lighting modes added

--- lights/lights.ino
+++ lights/lights.ino
@@ -1,23 +1,81 @@
-int outputPin = 3;
-int LIGHT_ON = 150;
-int LIGHT_OFF = 0;
-int pause = 2000;
+#define OUT_PIN_1 3
+#define OUT_PIN_2 5
+#define LIGHT_ON 150
+#define LIGHT_OFF 0
+
+int delayPeriod = 1000;
 
 void setup()
 {
-  pinMode(outputPin, OUTPUT);
+  pinMode(OUT_PIN_1, OUTPUT);
+  pinMode(OUT_PIN_2, OUTPUT);
 }
 
 void loop()
 {
-//  for (int i=0; i <= 200; i += 10)
-//  {
-//    analogWrite(outputPin, i);
-//    delay(50);
-//  }
-//  delay(300);
-    analogWrite(outputPin, LIGHT_ON);
-    delay(pause);
-    analogWrite(outputPin, LIGHT_OFF);
-    delay(pause);
+//  int num = random(1, 10);
+  flash2(3, delayPeriod);
+  flash(10, delayPeriod);
+  flash3(10, delayPeriod);
 }
+
+void flash(int numFlashes, int d)
+{
+  for (int i = 0; i < numFlashes; i++)
+  {
+    analogWrite(OUT_PIN_1, LIGHT_ON);
+    delay(d);
+    analogWrite(OUT_PIN_2, LIGHT_ON);
+    delay(d);
+    analogWrite(OUT_PIN_1, LIGHT_OFF);
+    delay(d);
+    analogWrite(OUT_PIN_2, LIGHT_OFF);
+//    delay(d);
+  }
+}
+
+void flash2(int numFlashes, int d)
+{
+  for (int k = 0; k < numFlashes; k++)
+  {
+    for (int i = 0; i <= LIGHT_ON; i += 5)
+    {
+      analogWrite(OUT_PIN_1, i);
+      analogWrite(OUT_PIN_2, i);
+      delay(100);
+    }
+    delay(d);  
+    for (int i = LIGHT_ON; i >= 0; i -= 5)
+    {
+      analogWrite(OUT_PIN_1, i);
+      analogWrite(OUT_PIN_2, i);
+      delay(200);
+    }
+    delay(d);
+  }
+}
+
+void flash3(int numFlashes, int d)
+{
+  for (int i = 0; i < 3; i++)
+  {
+    analogWrite(OUT_PIN_1, LIGHT_ON);
+    analogWrite(OUT_PIN_2, LIGHT_ON);
+    delay(500);
+    analogWrite(OUT_PIN_1, LIGHT_OFF);
+    analogWrite(OUT_PIN_2, LIGHT_OFF);
+    delay(500);
+  }
+  delay(500);
+  for (int i = 0; i < 6; i++)
+  {
+    analogWrite(OUT_PIN_1, LIGHT_ON);
+    analogWrite(OUT_PIN_2, LIGHT_ON);
+    delay(200);
+    analogWrite(OUT_PIN_1, LIGHT_OFF);
+    analogWrite(OUT_PIN_2, LIGHT_OFF);
+    delay(200);
+  }
+  delay(500);
+}
+
