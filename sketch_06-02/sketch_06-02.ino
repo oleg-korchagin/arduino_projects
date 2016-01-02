aa0d89649eee69386c39151f0517f145603c89cf
Oleg Korchagin <OKorchagin@revelsystems.com>
2016-01-02 13:21:44 +0300

    sketch 6 02 03

---
+++ sketch_06-02/sketch_06-02.ino
@@ -0,0 +1,16 @@
+int inputPin = 5;
+
+void setup()
+{
+  pinMode(inputPin, INPUT);
+  digitalWrite(inputPin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int reading = digitalRead(inputPin);
+  Serial.println(reading);
+  delay(500);
+}
+
