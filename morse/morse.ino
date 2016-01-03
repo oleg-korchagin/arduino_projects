5a2d2c48a9de17962692e0feffddbc06ccc470ec
Oleg Korchagin <OKorchagin@revelsystems.com>
2016-01-03 12:33:56 +0300

    Morse code sketch

---
+++ morse/morse.ino
@@ -0,0 +1,62 @@
+#define DOT_DELAY 200
+#define LED_PIN 3
+
+char* letters[] = {
+  ".-", "-...", "-.-.", "-..", ".",  // A-I
+  "..-", "--.", "....", "..",
+  ".---", "-.-", ".-..", "--", "-.", // J-R
+  "---", ".--.", "--.-", ".-.",
+  "...", "-", "..-", "...-", ".--",  // S-Z
+  "-..-", "-.--", "--.."
+};
+
+char* numbers[] = {
+  "-----", ".----", "..---", "...--", "....-",
+  ".....", "-....", "--...", "---..", "----."
+};
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char ch;
+  if (Serial.available() > 0) {
+    ch = Serial.read();
+    
+    if (ch >= 'a' && ch <= 'z') {
+      flashSequence(letters[ch - 'a']);
+    } else if (ch >= 'A' && ch <= 'Z') {
+      flashSequence(letters[ch - 'A']);
+    } else if (ch >= '0' && ch <= '9') {
+      flashSequence(letters[ch - '0']);
+    } else if (ch == ' ') {
+      delay(DOT_DELAY);
+    }
+  }
+}
+
+void flashSequence(char* sequence)
+{
+  int i = 0;
+  while (sequence[i] != '\0') {
+    flashDotOrDash(sequence[i]);
+    i++;
+  }
+  delay(DOT_DELAY * 3);
+}
+
+void flashDotOrDash(char dotOrDash)
+{
+  digitalWrite(LED_PIN, HIGH);
+  if (dotOrDash == '.') {
+    delay(DOT_DELAY);
+  } else {
+    delay(DOT_DELAY * 3);
+  }
+  digitalWrite(LED_PIN, LOW);
+  delay(DOT_DELAY);
+}
