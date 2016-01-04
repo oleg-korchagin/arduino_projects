31feded53ca787c09146e54051c522ee71296005
Oleg Korchagin <OKorchagin@revelsystems.com>
2016-01-04 12:52:02 +0300

    Morse coder moved to the library

--- lights/lights.ino
+++ lights/lights.ino
@@ -1,24 +1,13 @@
+#include <MorseCoder.h>
+
 #define OUT_PIN_1 3
 #define OUT_PIN_2 5
-#define LIGHT_ON 200
+#define LIGHT_ON 255
 #define LIGHT_OFF 0
 #define DOT_DELAY 200
 
 int delayPeriod = 1000;
-
-char* letters[] = {
-  ".-", "-...", "-.-.", "-..", ".",  // A-I
-  "..-", "--.", "....", "..",
-  ".---", "-.-", ".-..", "--", "-.", // J-R
-  "---", ".--.", "--.-", ".-.",
-  "...", "-", "..-", "...-", ".--",  // S-Z
-  "-..-", "-.--", "--.."
-};
-
-char* numbers[] = {
-  "-----", ".----", "..---", "...--", "....-", // 0-4
-  ".....", "-....", "--...", "---..", "----."  // 5-9
-};
+int led_pins[] = { OUT_PIN_1, OUT_PIN_2 };
 
 char* messages[] = {
   "HAPPY NEW YEAR",
@@ -27,27 +16,18 @@ char* messages[] = {
   "SODOMY IS SIN"
 };
 
+MorseCoder morseCoder(led_pins, DOT_DELAY);
+
 void setup()
 {
-  pinMode(OUT_PIN_1, OUTPUT);
-  pinMode(OUT_PIN_2, OUTPUT);
+  randomSeed(analogRead(0));
 }
 
 void loop()
 {
-//  int num = random(1, 10);
   flash2(3, delayPeriod);
   flash(10, delayPeriod);
-//  flash3(10, delayPeriod);
-  flash_morse(messages[random(sizeof(messages))]);
-}
-
-void flash_morse(char* message) {
-  int i = 0;
-  while (message[i] != '\0') {
-    flashChar(message[i]);
-    i++;
-  }
+  morseCoder.flashMessage(messages[random(sizeof(messages))]);
 }
 
 void flash(int numFlashes, int d)
@@ -61,7 +41,6 @@ void flash(int numFlashes, int d)
     analogWrite(OUT_PIN_1, LIGHT_OFF);
     delay(d);
     analogWrite(OUT_PIN_2, LIGHT_OFF);
-//    delay(d);
   }
 }
 
@@ -73,76 +52,16 @@ void flash2(int numFlashes, int d)
     {
       analogWrite(OUT_PIN_1, i);
       analogWrite(OUT_PIN_2, i);
-      delay(100);
+      delay(50);
     }
     delay(d);  
     for (int i = LIGHT_ON; i >= 0; i -= 5)
     {
       analogWrite(OUT_PIN_1, i);
       analogWrite(OUT_PIN_2, i);
-      delay(200);
+      delay(100);
     }
     delay(d);
   }
 }
 
-//void flash3(int numFlashes, int d)
-//{
-//  for (int i = 0; i < 3; i++)
-//  {
-//    analogWrite(OUT_PIN_1, LIGHT_ON);
-//    analogWrite(OUT_PIN_2, LIGHT_ON);
-//    delay(500);
-//    analogWrite(OUT_PIN_1, LIGHT_OFF);
-//    analogWrite(OUT_PIN_2, LIGHT_OFF);
-//    delay(500);
-//  }
-//  delay(500);
-//  for (int i = 0; i < 6; i++)
-//  {
-//    analogWrite(OUT_PIN_1, LIGHT_ON);
-//    analogWrite(OUT_PIN_2, LIGHT_ON);
-//    delay(200);
-//    analogWrite(OUT_PIN_1, LIGHT_OFF);
-//    analogWrite(OUT_PIN_2, LIGHT_OFF);
-//    delay(200);
-//  }
-//  delay(500);
-//}
-
-void flashChar(char ch)
-{
-    if (ch >= 'a' && ch <= 'z') {
-      flashSequence(letters[ch - 'a']);
-    } else if (ch >= 'A' && ch <= 'Z') {
-      flashSequence(letters[ch - 'A']);
-    } else if (ch >= '0' && ch <= '9') {
-      flashSequence(letters[ch - '0']);
-    } else if (ch == ' ') {
-      delay(DOT_DELAY);
-    }
-}
-
-void flashSequence(char* sequence)
-{
-  int i = 0;
-  while (sequence[i] != '\0') {
-    flashDotOrDash(sequence[i]);
-    i++;
-  }
-  delay(DOT_DELAY * 3);
-}
-
-void flashDotOrDash(char dotOrDash)
-{
-  digitalWrite(OUT_PIN_1, HIGH);
-  digitalWrite(OUT_PIN_2, HIGH);
-  if (dotOrDash == '.') {
-    delay(DOT_DELAY);
-  } else {
-    delay(DOT_DELAY * 3);
-  }
-  digitalWrite(OUT_PIN_1, LOW);
-  digitalWrite(OUT_PIN_2, LOW);
-  delay(DOT_DELAY);
-}
--- morse/morse.ino
+++ morse/morse.ino
@@ -1,23 +1,14 @@
+#include <MorseCoder.h>
+
 #define DOT_DELAY 200
 #define LED_PIN 3
 
-char* letters[] = {
-  ".-", "-...", "-.-.", "-..", ".",  // A-I
-  "..-", "--.", "....", "..",
-  ".---", "-.-", ".-..", "--", "-.", // J-R
-  "---", ".--.", "--.-", ".-.",
-  "...", "-", "..-", "...-", ".--",  // S-Z
-  "-..-", "-.--", "--.."
-};
+int led_pins[] = { LED_PIN };
 
-char* numbers[] = {
-  "-----", ".----", "..---", "...--", "....-", // 0-4
-  ".....", "-....", "--...", "---..", "----."  // 5-9
-};
+MorseCoder morseCoder(led_pins, DOT_DELAY);
 
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -26,41 +17,7 @@ void loop()
   char ch;
   if (Serial.available() > 0) {
     ch = Serial.read();
-    flashChar(ch);
+    morseCoder.flashChar(ch);
   }
 }
 
-void flashChar(char ch)
-{
-    if (ch >= 'a' && ch <= 'z') {
-      flashSequence(letters[ch - 'a']);
-    } else if (ch >= 'A' && ch <= 'Z') {
-      flashSequence(letters[ch - 'A']);
-    } else if (ch >= '0' && ch <= '9') {
-      flashSequence(letters[ch - '0']);
-    } else if (ch == ' ') {
-      delay(DOT_DELAY);
-    }
-}
-
-void flashSequence(char* sequence)
-{
-  int i = 0;
-  while (sequence[i] != '\0') {
-    flashDotOrDash(sequence[i]);
-    i++;
-  }
-  delay(DOT_DELAY * 3);
-}
-
-void flashDotOrDash(char dotOrDash)
-{
-  digitalWrite(LED_PIN, HIGH);
-  if (dotOrDash == '.') {
-    delay(DOT_DELAY);
-  } else {
-    delay(DOT_DELAY * 3);
-  }
-  digitalWrite(LED_PIN, LOW);
-  delay(DOT_DELAY);
-}
